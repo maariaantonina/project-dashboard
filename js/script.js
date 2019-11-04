ecdfd5dac2591eb8e0a1db9630795eb5c8f6d167
Maria Kasińska <mariakasinska17@gmail.com>
2019-11-04 21:15:16 +0100

    validate email address

--- js/script.js
+++ js/script.js
@@ -10,4 +10,18 @@
     toggleMenu();
   });
 
+  document.querySelector('form').addEventListener('submit', function(event) {
+  let isFormValidate = true;
+  console.log(isFormValidate);
+
+  const emailAddressInput = event.target.querySelector('input[name="email_address"]')
+  if(!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(emailAddressInput))) {
+    isFormValidate = false;
+    console.log(isFormValidate);
+    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Wrong email address';
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+})
+
 }
